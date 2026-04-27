dd159c6ad2f6e230b859c98411ac3bb65861a0bc
TestPrograms <basavrajk.295@gmail.com>
2026-04-27 14:08:27 +0530

    Test

---
+++ practice/ValidParenthesis.java
@@ -0,0 +1,26 @@
+package practice;
+
+import java.util.Stack;
+
+public class ValidParenthesis {
+    public static boolean isValid(String s){
+        Stack<Character> stack = new Stack<>();
+        for(char ch: s.toCharArray()){
+            if(ch == '(' || ch == '{' || ch == '['){
+                stack.push(ch);
+            } else {
+                if(stack.isEmpty()) return false;
+                char top = stack.pop();
+                if((ch == ')' && top != '(')||
+                        (ch == '}' && top != '{')||
+                        (ch == ']' && top != '[')){
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+    static void main(String[] args) {
+        System.out.println(isValid("{[()]}"));
+    }
+}
